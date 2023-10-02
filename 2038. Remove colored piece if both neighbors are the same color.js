95fff2d01c70a535bb1008ae7d384b33ca16ec53
Navin Kumaran <navinkumaran2004@gmail.com>
2023-10-02 14:37:51 +0530

    2038

--- 2038. Remove colored piece if both neighbors are the same color.js
+++ 2038. Remove colored piece if both neighbors are the same color.js
@@ -26,3 +26,41 @@ var winnerOfGame = function (colors) {
 let result = winnerOfGame("AAAABBBB");
 console.log(result);
 
+
+/*
+bool winnerOfGame(char * colors){
+    int len = strlen(colors);
+    int count_a = 0,count_b = 0;
+    for(int i;i<len;i++){
+        char x = colors[i];
+        int count = 0;
+        while(i<len && colors[i]==x){
+            i++;
+            count++;
+        }
+        if(x=='A'){
+            count_a += (count -2 > 0) ? count-2 : 0;
+        }
+        else if(x=='B'){
+            count_b += (count -2 > 0) ? count-2 : 0;
+        }
+        i--;
+    }
+    return count_a > count_b;
+}
+*/
+
+/*
+class Solution(object):
+    def winnerOfGame(self, colors):
+        """
+        :type colors: str
+        :rtype: bool
+        """
+        c = collections.Counter()
+        for x,t in groupby(colors):
+            c[x] += max(len(list(t))-2,0)
+        if c['A'] > c['B']:
+            return True
+        return False
+*/
