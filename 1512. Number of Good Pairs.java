fad60a9cf252044e065e9e25b7059be7bd72c186
Navin Kumaran <navinkumaran2004@gmail.com>
2023-10-03 17:53:43 +0530

    1512

---
+++ 1512. Number of Good Pairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,2,3,1,1,3};
+        int result = solution.numIdenticalPairs(nums);
+        System.out.println(result);      
+    }
+}
+
+
