8708644219a9f32f73dcf5e6ab7ea0ceda98d954
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-01 00:22:52 +0530

    1295

---
+++ 1295. Find Number With Even Number Of Digits.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count =0;
+        for (int num : nums) {
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean even(int num){
+        return digits(num) % 2 == 0;
+    }
+    static int digits(int num){
+        if (num < 0) {
+            num = num * -1;
+        }
+        return (int)(Math.log10(num)+1);
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = { 12, 345, 2, 6, 7896 };
+        int evenCount = sol.findNumbers(nums);
+        System.out.println("The number of integers with even number of digits is: " + evenCount);
+    }
+}
