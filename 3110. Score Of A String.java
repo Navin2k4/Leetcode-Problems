125be09ff1f6aef9b833f9edf86b959af4a6b701
Navin Kumaran <navinkumaran2004@gmail.com>
2024-06-02 10:07:31 +0530

    3110

---
+++ 3110. Score Of A String.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for (int i = 1; i < s.length(); i++) {
+            sum += Math.abs(((int) s.charAt(i - 1)) - ((int) s.charAt(i)));
+        }
+        return sum;
+    }
+}
